fe3876367ac61cb3ee62e014b733d76a889035bf
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-04-12 17:39:51 -0400

    Time: 8 ms (38.42%) | Memory: 19.4 MB (19.95%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,13 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
 
-        # nums_map = Counter(nums)
+        nums_map = Counter(nums)
 
-        # for num, count in nums_map.items():
-        #     if count > len(nums)/2:
-        #         return num
-        nums.sort()
-        return nums[len(nums) // 2]
+        for num, count in nums_map.items():
+            if count > len(nums)/2:
+                return num
+
 
 
         
