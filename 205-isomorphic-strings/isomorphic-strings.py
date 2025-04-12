dcd9266d9573c2223ee644b357b3f2d384fed71c
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-04-12 17:16:47 -0400

    Time: 12 ms (40.24%) | Memory: 15 MB (22.67%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,61 +1,16 @@
-class Solution:
-    def isIsomorphic(self, s: str, t: str) -> bool:
-        s_map = {char:[] for char in s}
-        t_map = {char:[] for char in t}
-
-        s_set = set(s)
-        t_set = set(t)
-
-        if len(s_set) != len(t_set):
-            return False
-
-        for idx, char in enumerate(s):
-            if char in s_map:
-                s_map[char].append(idx)
-        
-        for idx, char in enumerate(t):
-            if char in t_map:
-                t_map[char].append(idx)
-
-        return list(s_map.values()) == list(t_map.values())
-
-
-        #paper -> title
-        #paper -> ttile
-        
-        '''
-        TRAVERSE S
-        Store 
-        P-> [0, 2]
-        A -> [1]
-        E -> [3]
-        R -> [4]
-
-        TRAVERSE T
-        store
-        T->[0, 2]
-        I->[1]
-        L -> [3]
-        E -> [4]
-
-        Extract the values and compare
-        '''
-
-        ##"foo", t = "bar"
-        '''
-        {f:[0], o:[1,2]}
-        {b:[0], a:[1,2]}
-        '''
-
-
-        
-
-
-
-
-
-
-
-
-
+import math
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        s2t = {}
+        t2s = {}
+        for ss, tt in zip(s, t):
+            if ss not in s2t:
+                if tt in t2s:
+                    return False
+                s2t[ss] = tt
+                t2s[tt] = ss
+            else:
+                if s2t[ss] != tt:
+                    return False
+        return True
         
