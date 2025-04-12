6026ce5743813a0d6bf8ea5427f432e7e8f44285
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-04-12 17:50:31 -0400

    Time: 7 ms (56.45%) | Memory: 19.4 MB (19.95%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,11 +1,25 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-
+        # simple approach with O(n) time and space
+        '''
         nums_map = Counter(nums)
 
         for num, count in nums_map.items():
             if count > len(nums)/2:
                 return num
+        '''
+
+        # Boyer-Moore Voting Algorithm
+        # O(n) time O(1) space
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+
+        return candidate
 
 
             
