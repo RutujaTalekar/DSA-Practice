ed6b16ffcd4447b81d83a5bd19d786fff4ddd3e8
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-04-13 19:21:04 -0400

    Time: 3 ms (35.59%) | Memory: 19.8 MB (68.80%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -6,9 +6,46 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        # DFS Recursive with high low limits
+        inOrder = []
         # Inorder traversal of BST is in ascending order
+        def inOrderBST(node):
+            if not node:
+                return
+            if node.left:
+                inOrderBST(node.left)
+            inOrder.append(node.val)
+            # if len(inOrder) > 1 and inOrder[-1] < inOrder[-2]:
+            #     return False
+            if node.right:
+                inOrderBST(node.right)
+            
+
+
+
+
+            # if node.left and node.val < inOrderBST(node.left):
+            #     return False
+            # if node.right and node.val > inOrderBST(node.right):
+            #     return False
         
+        
+        if root:
+            inOrderBST(root)
+            prev = inOrder[0]
+            for i in range(1, len(inOrder)):
+                if inOrder[i] <= prev:
+                    return False
+                prev = inOrder[i]
+            print(inOrder)
+        return True
+                
+
+
+
+
+
+        # DFS Recursive with high low limits
+        '''
         low = - math.inf 
         high = math.inf
 
@@ -25,4 +62,4 @@ class Solution:
             return True
         else:
             return validate(root.left, low, root.val) and validate(root.right, root.val, high)
-
+        '''
