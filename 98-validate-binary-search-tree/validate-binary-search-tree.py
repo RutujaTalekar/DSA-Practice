da85ae76f5fb8187388f8ba40db1c44615a4e2a6
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-04-13 19:32:48 -0400

    Time: 3 ms (35.59%) | Memory: 19.8 MB (31.58%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -6,46 +6,8 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        inOrder = []
-        # Inorder traversal of BST is in ascending order
-        def inOrderBST(node):
-            if not node:
-                return
-            if node.left:
-                inOrderBST(node.left)
-            inOrder.append(node.val)
-            # if len(inOrder) > 1 and inOrder[-1] < inOrder[-2]:
-            #     return False
-            if node.right:
-                inOrderBST(node.right)
-            
-
-
-
-
-            # if node.left and node.val < inOrderBST(node.left):
-            #     return False
-            # if node.right and node.val > inOrderBST(node.right):
-            #     return False
-        
-        
-        if root:
-            inOrderBST(root)
-            prev = inOrder[0]
-            for i in range(1, len(inOrder)):
-                if inOrder[i] <= prev:
-                    return False
-                prev = inOrder[i]
-            print(inOrder)
-        return True
-                
-
-
-
-
-
         # DFS Recursive with high low limits
-        '''
+
         low = - math.inf 
         high = math.inf
 
@@ -62,4 +24,29 @@ class Solution:
             return True
         else:
             return validate(root.left, low, root.val) and validate(root.right, root.val, high)
+
+
+        
+        # Inorder traversal of BST is in ascending order
+        '''
+        inOrder = []
+        def inOrderBST(node):
+            if not node:
+                return
+            if node.left:
+                inOrderBST(node.left)
+            inOrder.append(node.val)
+            if node.right:
+                inOrderBST(node.right)
+        
+        
+        if root:
+            inOrderBST(root)
+            prev = inOrder[0]
+            for i in range(1, len(inOrder)):
+                if inOrder[i] <= prev:
+                    return False
+                prev = inOrder[i]
+            print(inOrder)
+        return True
         '''
