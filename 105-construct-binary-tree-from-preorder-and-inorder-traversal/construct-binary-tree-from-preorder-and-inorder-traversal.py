f4e2702211c159602c36f88fa2be80e5dd2f4259
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-04-15 00:31:17 -0400

    Time: 4 ms (76.58%) | Memory: 19.2 MB (74.43%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,17 +6,50 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        # base conditions
+        # O(n^2) time but intuitive solution
+        '''
         if not preorder or not inorder:
             return
         
         root = TreeNode(preorder[0])
         mid = inorder.index(preorder[0])
-        # print(preorder,inorder)
         root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
         root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
 
         return root
+        '''
+
+        # better time complexity o(n), dont use slicing or index() - this takes o(n)
+        # What are we storing in map - values : index for inorder
+        # the values will be same in preorder, so this will give us the index of inorder vals
+        # when we are locating root vals in inorder
+        # instead of sending sliced lists, use indexes from map
+
+        inorder_map = {val:idx for idx, val in enumerate(inorder)}
+        self.pre_start = 0
+
+        def build(in_start, in_end):
+            if in_start > in_end:
+                return
+            
+            root = TreeNode(preorder[self.pre_start])
+            self.pre_start += 1
+            mid = inorder_map[root.val]
+
+            root.left = build(in_start, mid-1)
+            root.right = build(mid+1, in_end)
+
+            return root
+
+        return build(0, len(inorder)-1)
+
+        '''
+        Note - inorder_map	is read-only and defined in the outer scope, so Python allows access inside buildTreeHelper
+                pre_start on the other hand gets updated, if you try to modify a variable from an outer scope, you must declare it nonlocal or make it an attribute like self.pre_idx
+        '''
+
+            
+
 
             
             
