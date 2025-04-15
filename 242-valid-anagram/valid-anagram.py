f1415d59b227e756106d8bbc155c019c920567f5
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-04-15 17:29:00 -0400

    Time: 4 ms (94.39%) | Memory: 17.7 MB (86.43%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -6,23 +6,33 @@ class Solution:
         Check the same number of letters exists in the other map
         '''
 
-        # if len(s) != len(t):
-        #     return False
-
-        # s_map = Counter(s)
-
-        # for char in t:
-        #     if s_map[char] and s_map[char] > 0:
-        #         s_map[char] -=1
-        #     else:
-        #         return False
+        if len(s) != len(t):
+            return False
         
-        # return 0 == sum(s_map.values())
+        # another solution with sets
+        for cha in set(s): 
+            if s.count(cha)!=t.count(cha):
+                return False
+        return True
 
         # another solution with Counter maps
+        '''
         s_map = Counter(s)
         t_map = Counter(t)
         return s_map == t_map
+        '''
 
+        # draft 1 solution
+        '''
+        s_map = Counter(s)
+
+        for char in t:
+            if s_map[char] and s_map[char] > 0:
+                s_map[char] -=1
+            else:
+                return False
+        
+        return 0 == sum(s_map.values())
+        '''
         
         
