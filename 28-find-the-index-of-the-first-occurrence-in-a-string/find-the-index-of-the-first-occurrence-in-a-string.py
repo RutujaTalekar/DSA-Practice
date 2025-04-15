eb0b1dc2f703f0cba192dc1adb5f267a1c5330de
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-04-15 18:32:36 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (32.35%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,20 +1,19 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         '''
-        check len(needle) <= len(haystack)
+        Edge cases - 
+        if len(needle) > len(haystack), invalid
+        if len of both is same then check directly and return result
         '''
         need_len, hay_len = len(needle), len(haystack)
 
         if need_len > hay_len:
             return -1
-        
-        if need_len == hay_len and haystack == needle:
-            return 0
-        
+
         for i in range(0, hay_len - need_len + 1):
             res = i
             idx = 0
-            
+
             while idx < need_len and haystack[i] == needle[idx]:
                 i += 1
                 idx += 1
