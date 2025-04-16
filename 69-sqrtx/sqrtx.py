29cc4c59c2f6145c0e594bf26d33f61ccd2b8667
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-04-16 18:58:34 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (52.14%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -5,13 +5,20 @@ class Solution:
 
         if x < 2:
             return x
+
         while left <= right:
             mid = (left + right) // 2
             square = mid**2
-            if x >= square:
-                left = mid + 1
-            else:
+
+            if x == square:
+                return mid
+
+            if x < square:
                 right = mid - 1
+        
+            else:
+                left = mid + 1
+                
                 
         
         return right
