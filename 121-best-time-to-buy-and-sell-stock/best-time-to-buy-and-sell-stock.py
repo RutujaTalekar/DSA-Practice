65b5ceb9fc4fe6c17df125b570c840cf57585416
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-04-17 18:40:41 -0400

    Time: 159 ms (5.52%) | Memory: 30.4 MB (9.08%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,10 +1,56 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+
+
+        
+        #Accpted, finds daily profits and uses maximum subarray(dp) to find max profit
+        profits = []
+        if len(prices) <= 1:
+            return 0
+
+        for i in range(1,len(prices)):
+            profit = prices[i] - prices[i-1]
+            profits.append(profit)
+
+        maxi = 0
+        dp = [0]*len(profits)
+        dp[0] = profits[0]
+
+        for i in range(1,len(profits)):
+            dp[i] = max(profits[i], dp[i-1]+profits[i])
+
+        if max(dp) < 0:
+            return 0
+        return max(dp)
+
+        
+
+
+        # best = 0
+        # buy = prices[0]
+
+        # for sell in prices[1:]:
+        #     profit = sell-buy
+
+        #     if profit < 0:
+        #         buy = sell
+        #     elif profit>0 and profit>best:
+        #         best = profit
+        #     else:
+        #         pass
+                
+        # return best
+
+
+
+
+        '''
+        Brute force - Time complexity O(n), space complexity - O(1)
+
         maxProfit = 0
 
         if not prices:
             return 0
-        
         buy, sell, profit = prices[0], prices[0], 0
 
         for i in range(len(prices)):
@@ -19,6 +65,7 @@ class Solution:
             maxProfit = max(profit, maxProfit)
         
         return maxProfit
+        '''
 
 
 
