de59781a180842d6ac222bd23daa167e4e544b34
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-04-18 01:50:17 -0400

    Time: 263 ms (28.26%) | Memory: 34.4 MB (5.46%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -5,33 +5,58 @@ class Solution(object):
         :rtype: int
         """
 
-        visited = set()
-        rows = len(grid)
-        cols = len(grid[0])
+        # visited = set()
+        # rows = len(grid)
+        # cols = len(grid[0])
 
-        #iterative BFS
-        def bfsExplore(r,c):
+        # #iterative BFS
+        # def bfsExplore(r,c):
 
 
-            q = deque()
-            q.append((r,c))
+        #     q = deque()
+        #     q.append((r,c))
 
-            while q:
-                r,c = q.popleft()
-                if (r,c) not in visited and r < rows and c < cols and r >= 0 and c >=0 and grid[r][c] == '1':
-                    grid[r][c] = '0'
-                    q.append((r+1,c))
-                    q.append((r-1,c))
-                    q.append((r,c+1))
-                    q.append((r,c-1))
+        #     while q:
+        #         r,c = q.popleft()
+        #         if (r,c) not in visited and r < rows and c < cols and r >= 0 and c >=0 and grid[r][c] == '1':
+        #             grid[r][c] = '0'
+        #             q.append((r+1,c))
+        #             q.append((r-1,c))
+        #             q.append((r,c+1))
+        #             q.append((r,c-1))
             
+        #     return True
+        # res = 0
+        # for i in range(rows):
+        #     for j in range(cols):
+        #         if grid[i][j] == '1':
+        #             bfsExplore(i,j)
+        #             res+=1
+        
+        # return res
+
+        # recursive DFS
+        rows = len(grid)
+        cols= len(grid[0])
+        visitedIsland = set()
+        def dfsExplore(r,c):
+            if r >= rows or c >= cols or r < 0 or c < 0 or (r,c) in visitedIsland or grid[r][c] == '0':
+                return False
+
+            visitedIsland.add((r,c))
+
+            dfsExplore(r+1, c)
+            dfsExplore(r-1, c)
+            dfsExplore(r, c-1)           
+            dfsExplore(r, c+1)
+
             return True
+        
         res = 0
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == '1':
-                    bfsExplore(i,j)
-                    res+=1
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1' and (r,c) not in visitedIsland and dfsExplore(r,c) == True:
+                    res +=1
         
         return res
                 
