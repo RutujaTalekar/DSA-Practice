e337ad1a928ab718eb4d78b0e31ff0397075e79a
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-04-19 12:06:10 -0400

    Time: 2468 ms (5.03%) | Memory: 21.7 MB (36.83%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -5,34 +5,29 @@ class Solution:
         # sort alphabetically each string in strs
         # store in sorted_strs
         # use map to compare k:v k -> sorted key, v -> groups of strngs
-        res = []
 
+        
+        res = []
         strs_map = {idx:string for idx, string in enumerate(strs)}
-        print(strs_map)
         sorted_strs = list()
 
         for idx, string in enumerate(strs):
             sorted_chars = sorted(string)
-            sorted_strs.append("".join(sorted_chars))
-        print(sorted_strs)
-        
+            sorted_strs.append("".join(sorted_chars))        
 
         keys = set(sorted_strs)
-        print(keys)
 
         for key in keys:
             temp = []
             for idx in range(len(sorted_strs)):
                 if key == sorted_strs[idx]:
                     temp.append(strs_map[idx])
-                
             res.append(temp)
         
         return res
             
 
-            
-        # for sorted_str in sorted_strs:
+        
 
 
 
