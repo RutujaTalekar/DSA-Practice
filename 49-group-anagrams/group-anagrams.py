f67f1cea9185f9391086f13baa86e7df89bd2526
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-04-19 12:26:23 -0400

    Time: 11 ms (83.04%) | Memory: 20.8 MB (63.25%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,17 +1,45 @@
-class Solution(object):
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-        strs_table = {}
-        for string in strs:
-            sorted_string = ''.join(sorted(string))
-
-            if sorted_string not in strs_table:
-                strs_table[sorted_string] = []
-
-            strs_table[sorted_string].append(string)
-
-        return list(strs_table.values())
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dict_map = defaultdict(list)
+        for char in strs:
+            key = ''.join(sorted(char))
+            dict_map[key].append(char)
+        return list(dict_map.values())
         
+        
+        # res = []
+        # strs_map = {idx:string for idx, string in enumerate(strs)}
+        # sorted_strs = list()
+
+        
+
+        # for idx, string in enumerate(strs):
+        #     sorted_chars = sorted(string)
+        #     sorted_strs.append("".join(sorted_chars))        
+
+        # keys = set(sorted_strs)
+
+        # for key in keys:
+        #     temp = []
+        #     for idx in range(len(sorted_strs)):
+        #         if key == sorted_strs[idx]:
+        #             temp.append(strs_map[idx])
+        #     res.append(temp)
+        
+        # print(strs_map)
+        # print(sorted_strs)
+        
+        # return res
+        
+            
+
+        
+
+
+
+
+
+
+
+
+
