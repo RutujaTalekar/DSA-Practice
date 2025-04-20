bc139ddb0a9ea7caee03adb5c98fc54f59ebf889
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-04-20 18:57:10 -0400

    Time: 40 ms (38.89%) | Memory: 17.6 MB (92.70%) - LeetSync

--- 278-first-bad-version/first-bad-version.py
+++ 278-first-bad-version/first-bad-version.py
@@ -5,42 +5,31 @@ class Solution:
     def firstBadVersion(self, n: int) -> int:
         
         '''
-        F F T T T T T T
+        F F F T T T T T
         1 2 3 4 5 6 7 8
 
         F F F F F F T T
         1 2 3 4 5 6 7 8
         '''
-
         
-        # left,right = 1, n
-        # while(left<=right):
-        #     mid = (left + right)//2
 
-        #     if isBadVersion(mid) == True:
-        #         if isBadVersion(mid-1) == False:
-        #             return mid
-        #         right = mid - 1
+        # Intuitive binary search solution
+        left,right = 1, n
+        while(left<=right):
+            mid = (left + right)//2
+
+            if isBadVersion(mid) == True:
+                if isBadVersion(mid-1) == False:
+                    return mid
+                right = mid - 1
             
-        #     if isBadVersion(mid) == False:
-        #         left = mid + 1
-
-        # return -1
-
-
-
-        left = 1
-        right = n
-
-        while left < right:
-            mid = (left + right) // 2
-
-            if isBadVersion(mid):
-                right = mid
-            else:
+            if isBadVersion(mid) == False:
                 left = mid + 1
-        
-        return right
+
+        return -1
+
+
+       
 
 
 
