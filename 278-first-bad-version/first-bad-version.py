14b5bd49d7441268fcbb855f004cb9eebe12802e
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-04-20 18:57:15 -0400

    Time: 40 ms (38.89%) | Memory: 17.7 MB (68.87%) - LeetSync

--- 278-first-bad-version/first-bad-version.py
+++ 278-first-bad-version/first-bad-version.py
@@ -12,6 +12,19 @@ class Solution:
         1 2 3 4 5 6 7 8
         '''
         
+        # Optimized binary search solution
+        left = 1
+        right = n
+
+        while left < right:
+            mid = (left + right) // 2
+
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+        
+        return left
 
         # Intuitive binary search solution
         left,right = 1, n
