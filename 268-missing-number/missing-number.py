213dcf43111dd87c70e6f69a82c4c5b35bfea320
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-04-20 17:27:29 -0400

    Time: 60 ms (16.39%) | Memory: 18.6 MB (73.34%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,15 +1,32 @@
 class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
+    # def missingNumber(self, nums: List[int]) -> int:
 
-        # O(n) time and o(n) space
-        nums_set = set(nums)
-        for i in range(len(nums)+1):
-            if i not in nums_set:
-                return i
-        return -1
+    #     # O(n) time and o(n) space
+    #     nums_set = set(nums)
+    #     for i in range(len(nums)+1):
+    #         if i not in nums_set:
+    #             return i
+    #     return -1
                 
-        # O(n) time and O(1) space
-        res = len(nums)
-        for i in range(len(nums)):
-            res+=i-nums[i]
-        return res
+    #     # O(n) time and O(1) space
+    #     res = len(nums)
+    #     for i in range(len(nums)):
+    #         res+=i-nums[i]
+    #     return res
+    def missingNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        def search(num):
+            left=0
+            right=len(nums)-1
+            while left<=right:
+                middle=(left+right)//2
+                if nums[middle]==num:
+                    return True
+                elif num>middle:
+                    left=middle+1
+                else:
+                    right=middle-1
+            return False
+        for i in range(0,len(nums)+1):
+            if not search(i):
+                return i
