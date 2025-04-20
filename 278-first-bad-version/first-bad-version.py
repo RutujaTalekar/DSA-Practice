95a7b368a94662cbb9104b1024d032549fa513cd
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-04-20 18:18:52 -0400

    Time: 40 ms (38.89%) | Memory: 17.9 MB (27.01%) - LeetSync

--- 278-first-bad-version/first-bad-version.py
+++ 278-first-bad-version/first-bad-version.py
@@ -3,6 +3,20 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
+        left = 1
+        right = n
+
+        while left < right:
+            mid = (left + right) // 2
+
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+        
+        return left
+
+
         '''
         F F T T T T T T
         1 2 3 4 5 6 7 8
@@ -11,42 +25,16 @@ class Solution:
         1 2 3 4 5 6 7 8
         '''
 
-        # left,right = 1, n
-        # while(left<=right):
-        #     mid = (left + right)//2
+        left,right = 1, n
+        while(left<=right):
+            mid = (left + right)//2
 
-        #     if isBadVersion(mid) == True:
-        #         if isBadVersion(mid-1) == False:
-        #             return mid
-        #         right = mid - 1
+            if isBadVersion(mid) == True:
+                if isBadVersion(mid-1) == False:
+                    return mid
+                right = mid - 1
             
-        #     if isBadVersion(mid) == False:
-        #         left = mid + 1
+            if isBadVersion(mid) == False:
+                left = mid + 1
 
-        # return - 1
-
-
-        l = 1
-        r = n
-        while l < r : 
-            mid = (l+r) // 2
-            if isBadVersion(mid):
-                r = mid
-            else:
-                l =  mid + 1
-        return l
-
-            
-
-
-
-
-
-
-
-        
-
-
-
-        
-        
+        return - 1
