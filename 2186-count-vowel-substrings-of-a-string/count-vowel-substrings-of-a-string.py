1327a2a2dda2f8025ece9e957b57aa5e8a9d7341
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-04-22 19:38:07 -0400

    Time: 83 ms (17.45%) | Memory: 17.7 MB (55.47%) - LeetSync

--- 2186-count-vowel-substrings-of-a-string/count-vowel-substrings-of-a-string.py
+++ 2186-count-vowel-substrings-of-a-string/count-vowel-substrings-of-a-string.py
@@ -9,24 +9,39 @@ class Solution:
         check if all five vowels are present
         if both true then increase counter
         '''
-        # sliding window pointers
+
+        # i, j = 0, 0
+        # vowels = {'a','e','i','o','u'}
+        # result = 0
+        
+        # for i in range(len(word)-4):
+        #     j = i+4
+        #     while j < len(word) and word[j] in vowels:
+        #         window = word[i:j+1]
+        #         if set(window) == vowels:
+        #             result +=1
+        #         j+=1
+        
+        # return result
+
+
         i, j = 0, 0
         vowels = {'a','e','i','o','u'}
         result = 0
-        
         for i in range(len(word)-4):
-            j = i+4
-            while j < len(word) and word[j] in vowels:
-                window = word[i:j+1]
-                if set(window) == vowels:
+            substring = ''
+            for j in range(i, len(word)):
+                if word[j] not in vowels:
+                    break
+                substring += word[j]
+                if len(substring) >= 5 and set(substring) == vowels:
                     result +=1
-                j+=1
+
         
         return result
 
-        # lookup = {idx:char for idx,char in enumerate(word)}
-        # a e i o u u
-        # 0 1 2 3 4 5
+
+
             
 
 
