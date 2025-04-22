fa177379c3a8cc3fd0242b098a6a9a28fcf3e992
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-04-22 00:42:25 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (26.40%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,29 +1,72 @@
-class Solution(object):
-    def merge(self, nums1, m, nums2, n):
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: None Do not return anything, modify nums1 in-place instead.
+        Do not return anything, modify nums1 in-place instead.
+        
         """
-       
-        i = m-1 
-        j = n-1
-        k = m+n-1
 
-        while (i >= 0 and j >= 0):
-            if nums1[i] > nums2[j]:
-                nums1[k] = nums1[i] 
+        '''
+        compare each element one by one of nums1 and nums2, untill one of them is traversed completely
+        Add the rest of the items wherever left nums1/nums2
+        Above soln works but it needs extra temp list to store sorted list.
+
+        To do it in O(1) space and to follow the conditions given, we can try adding from the end.
+        We are already left the amount of space we would need.
+        so compare last element of nums1 and nums2 with each other, and add the largest at end, do it for all elements.
+        '''
+
+        # we need pointers to get last element of nums1, nums2 and position of insert from end of nums1
+        i, j = m-1, n-1
+        insert_at = m+n-1
+
+        while i >= 0 and j >= 0:
+            if nums1[i] >= nums2[j]:
+                nums1[insert_at] = nums1[i]
                 i -=1
             else:
-                nums1[k] = nums2[j]
+                nums1[insert_at] = nums2[j]
                 j -=1
-            k -=1
+            insert_at -=1
         
+        # in case insert_at is not reached 0, and nums2 has elements left, then we can directly add them
         while j >= 0:
-            nums1[k] = nums2[j]
+            nums1[insert_at] = nums2[j]
             j -= 1
-            k -= 1
+            insert_at -=1
+
+        # in case insert_at is not reached 0, and nums2 has elements left, then we can directly add them
+        while i >= 0:
+            nums1[insert_at] = nums1[i]
+            i -= 1
+            insert_at -=1
+        
+
+        # in case nums1 doesnt have any elements of its own
+        if m+n == n:
+            nums1[:] = nums2[:]
+
+
+    
+
+
+
+
+
+        
+
+                
+            
+
+            
+
+                
+            
+
+        
+
+
+
+
+            
 
         
