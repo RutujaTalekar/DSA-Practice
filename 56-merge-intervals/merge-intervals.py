7933e5cb34d391a756dc9e2871d001d9ef9d8457
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-04-23 01:13:11 -0400

    Time: 13 ms (13.81%) | Memory: 21.9 MB (14.99%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.py
@@ -0,0 +1,30 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+
+        if len(intervals) == 1:
+            return intervals
+        
+        '''
+        traverse through intervals and compare end to next start, if it overlaps merge
+        '''
+
+        output = []
+
+        intervals.sort()
+        print(intervals)
+
+        prev_s, prev_e = intervals[0]
+        output.append([prev_s, prev_e])
+        for cur_s, cur_e in intervals[1:]:
+            if prev_e >= cur_s:
+                output.pop()
+                output.append([min(prev_s, cur_s), max(prev_e,cur_e)])
+            else:
+                output.append([cur_s, cur_e])
+            prev_s, prev_e = output[-1]
+
+        # if intervals[0][1] < intervals[1][0]:
+        #     output.insert(0, intervals[0])
+
+            
+        return output
