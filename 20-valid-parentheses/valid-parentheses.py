8776f18a96cdb32a9659e5bbe6accc9edc73aa92
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-04-24 18:28:32 -0400

    Time: 4 ms (9.19%) | Memory: 18 MB (10.31%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        lookup = {')':'(', '}':'{', ']':'['}
+        stack = []
+
+        if len(s) % 2 == 1:
+            return False
+
+        for char in s:
+            if char in lookup:
+                if stack and stack[-1] == lookup[char]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(char)
+        
+        return True if not stack else False
+            
+
+
+            
+                
+                
+            
+        
+        
