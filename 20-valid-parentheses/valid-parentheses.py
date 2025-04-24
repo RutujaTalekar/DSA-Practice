5637d8e5dc0ad9374a04f638deef7d63ed2782f6
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-04-24 18:36:47 -0400

    Time: 4 ms (9.19%) | Memory: 17.8 MB (51.05%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -13,6 +13,26 @@ class Solution:
                 stack.append(char)
         
         return True if not stack else False
+
+
+        
+        map = { ')':'(' , '}':'{' , ']':'['  }
+        stack = []
+
+        for key in s:
+            if key in map.values():
+                stack.append(key)
+
+            else:
+                if stack:
+                    if stack[-1]!= map[key]:
+                        return False
+                    stack.pop() #matching pair\
+                else:
+                    return False
+
+        return True if not stack else False
+        
             
 
 
