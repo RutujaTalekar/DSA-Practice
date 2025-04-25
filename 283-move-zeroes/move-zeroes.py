008ec7d9e40f5a0f3d864ace98663d39bcd303cf
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-04-25 01:06:54 -0400

    Time: 12 ms (20.49%) | Memory: 19.1 MB (9.91%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -9,21 +9,11 @@ class Solution:
         count0 = 0
         while i< n-count0:
             if nums[i] == 0:
-                # nums.pop(i)
-                nums[i:] = nums[i+1:]
+                nums.pop(i)
+                # nums[i:] = nums[i+1:] This takes more time, pop is better
                 nums.append(0)
                 count0 += 1
             else:
                 i += 1
         return nums
-
-        # n = len(nums)
-        # i = 0
-        # while i< n:
-        #     if nums[i] == 0:
-        #         nums[i:] = nums[i+1:]
-        #         nums.append(0)
-        #     i += 1
-        
-        # return nums
         
