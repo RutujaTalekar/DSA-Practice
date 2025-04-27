e54dd914886730f9f2adcf77a3640c3e4f8d3df0
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-04-27 14:48:12 -0400

    Time: 23 ms (32.48%) | Memory: 17.8 MB (67.26%) - LeetSync

--- 756-pour-water/pour-water.py
+++ 756-pour-water/pour-water.py
@@ -1,62 +1,55 @@
-class Solution(object):
-    def pourWater(self, heights, volume, k):
-        """
-        :type heights: List[int]
-        :type volume: int
-        :type k: int
-        :rtype: List[int]
-        """
+class Solution:
+    def pourWater(self, heights: List[int], volume: int, k: int) -> List[int]:
         '''
-        Logic - 
+        Intuition - 
         from the droplet fall index k, 
             go left if left height is lower
             go right if right height is lower
             if both at same level stay there
             break if staying there.
         '''
+
+        '''
+        IMP part of logic - 
+        If left is lower (<) - update best and move 
+        If left is same (==) just move, but don't update best. Keep moving cause there might be a lower value ahead.
+        If left is higher (>) stop moving 
+        Same applied for right movement
+        '''
+
         # edge cases
-        if k >= len(heights):
+        if k > len(heights):
             return -1
         if len(heights) == 1:
             return [heights[0] + volume]
+        
 
         for _ in range(volume):
-            idx = k
+            best = idx = k
 
-            # move left
-            best = k
-            while idx - 1 >= 0 and heights[idx-1] <= heights[idx]:
+            # check left
+            while idx-1 >= 0 and heights[idx-1] <= heights[idx]:
                 if heights[idx-1] < heights[idx]:
-                    best = idx-1
+                    best = (idx-1)
                 idx -= 1
 
-            if best != k:
+            # if we found a lower position on left, we can continue with next drop
+            if best != k: 
                 heights[best] += 1
                 continue
-
-            # move right
-            idx = k
-            best = k
-            while idx + 1 < len(heights) and heights[idx+1] <= heights[idx]:
+            
+            best = idx = k
+            # if not found on left, check right
+            while idx+1 < len(heights) and heights[idx+1] <= heights[idx]:
                 if heights[idx+1] < heights[idx]:
-                    best = idx+1
+                    best = (idx +1)
                 idx += 1
-
-            if best != k:
-                heights[best] += 1
-                continue
-
-            # stay
-            heights[k] += 1
+            
+            # if we found lower position on right, it will be in best, or else best will be the k position
+            heights[best] += 1
 
         return heights
 
-
-
-            
-            
-
-                
 
             
 
