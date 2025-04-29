5db90df92145201ca03c80074965ff0ca09663e9
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-04-29 19:11:59 -0400

    Time: 4 ms (75.57%) | Memory: 24 MB (3.41%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.py
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.py
@@ -12,19 +12,20 @@ class Solution(object):
         column_max = np.max(np.array(matrix), axis=0)
         return list(set(row_min) & set(column_max))
 
-        # res = []
-        # for i in range(len(matrix)):
-        #     min_val = matrix[i][0]
-        #     min_col = 0
-        #     for j in range(1, len(matrix[0])):
-        #         val = matrix[i][j]
-        #         if val < min_val:
-        #             min_val = val
-        #             min_col = j
-        #     for k in range(len(matrix)):
-        #         if matrix[k][min_col] > min_val:
-        #             break
-        #     else:
-        #         res.append(min_val)
-        # return res
+        res = []
+        for i in range(len(matrix)):
+            min_val = matrix[i][0]
+            min_col = 0
+            for j in range(1, len(matrix[0])):
+                val = matrix[i][j]
+                if val < min_val:
+                    min_val = val
+                    min_col = j
+            for k in range(len(matrix)):
+                if matrix[k][min_col] > min_val:
+                    break
+            else:
+                res.append(min_val)
+                break
+        return res
         
