528e151e566b1c023706d9f5e18021962928276d
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-04-30 18:55:25 -0400

    Time: 715 ms (30.39%) | Memory: 20.8 MB (23.12%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -2,70 +2,32 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         
         # N square using hash set
-        
         result = set()
         nums.sort()
 
         for i in range(len(nums)):
             seen = set()
             target = -nums[i]
-            if i > 0 and nums[i] == nums[i - 1]:
+            if i > 0 and nums[i] == nums[i - 1]:    # deduplication logic
                 continue  # Optional: skip duplicate 'i' values
             for j in range(i+1, len(nums)):
                 alternate = target - nums[j]
                 if alternate in seen:
-                    triplets = tuple([nums[i], nums[j], alternate])
+                    triplets = tuple([nums[i], nums[j], alternate]) # we dont need to user sorted list as we are doing deduplication logic
                     result.add(triplets)
                 seen.add(nums[j])
         return list(result)
-        
-        
 
-
-        # O(n square) | sorting + two sum 
-        # result = []
-        # nums.sort()
+        # try using sorted array and two pointers
 
 
 
-        # for i in range(len(nums)-2):
-        #     left, right = i + 1 , len(nums) - 1
-        #     while left < right:
-        #         target_sum = nums[i] + nums[left] + nums[right]
-
-        #         if target_sum > 0:
-        #             right -= 1
-        #         if target_sum < 0:
-        #             left += 1
-        #         else:
-        #             result.append([nums[i], nums[left], nums[right]])
-        #             left += 1
-        #             right -= 1
-        #             while left < right and nums[left] == nums[left -1]:
-        #                 left +=1
-        
-        # return result
-
-
-                
-
-        
-        # N square using Hashmap
-        
-        lookup = {num:i for i, num in enumerate(nums)}
-        result = set()
-        nums.sort()
-
-        for i in range(len(nums)):
-            target = 0 - nums[i]
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue  # Optional: skip duplicate 'i' values
-            # two sum logic
-            for j in range(i+1, len(nums)):
-                new_target = target - nums[j]
-                if new_target in lookup and lookup[new_target] not in (i,j):
-                    result.add(tuple(sorted([nums[i], nums[j], new_target])))
-        
-        return list(result)
+        '''
+        Note -  For 3 sum problem, hashmap storage doesnt work as opposed to 2 sum
+                Because there can be duplicates in the list of numbers!!! 
+                When you do this - lookup = {num:i for i, num in enumerate(nums)}
+                it saves the index of last occurrence of that number, giving us false data
+                Imp to check constraints!!
+        '''
         
 
