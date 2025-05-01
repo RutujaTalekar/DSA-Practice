ac1636ff509728d5fbf1d293536b15f58a584989
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-05-01 00:11:22 -0400

    Time: 1509 ms (10.61%) | Memory: 20.3 MB (90.03%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,33 +1,61 @@
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
 class Solution:
-    def threeSum(self, nums):
-        a=[]
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # sorted array and two pointers
+        result = set()
         nums.sort()
+
         for i in range(len(nums)-2):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            j,k = i+1,len(nums)-1
-            while j<k:
-                s=nums[i] + nums[j] + nums[k]
-                if s < 0:
-                    j+=1
-                elif s > 0:
-                    k-=1
+
+            lo, hi = i+1, len(nums)-1
+            while (lo < hi):
+                sum_target = nums[i] + nums[lo] + nums[hi]  # ideally equal to 0
+
+                if sum_target > 0 :
+                    hi -=1
+                elif sum_target < 0:
+                    lo +=1
                 else:
-                    a.append([nums[i],nums[j],nums[k]])
-                    j += 1
-                    k -= 1
-                    while j < len(nums)-1 and nums[j] == nums[j - 1]:
-                        j += 1
-        return a
-
-            
+                    result.add((nums[i], nums[lo], nums[hi]))
+                    # print(result)
+                    lo +=1
+                    hi -=1
         
-            
+        return list(result)
 
 
+
+
+
+
+        # N square using hash set
+        '''
+        result = set()
+        nums.sort()
+
+        for i in range(len(nums)):
+            seen = set()
+            target = -nums[i]
+            if i > 0 and nums[i] == nums[i - 1]:    # deduplication logic
+                continue  # skips duplicate 'i' values, cause we have already found those triplets
+            for j in range(i+1, len(nums)):
+                alternate = target - nums[j]
+                if alternate in seen:
+                    triplets = tuple([nums[i], nums[j], alternate]) # we dont need to user sorted list as we are doing deduplication logic
+                    result.add(triplets)
+                seen.add(nums[j])
+        return list(result)
+        '''
 
         
-    
-            
+
+
+
+        '''
+        Note -  For 3 sum problem, hashmap storage doesnt work as opposed to 2 sum
+                Because there can be duplicates in the list of numbers!!! 
+                When you do this - lookup = {num:i for i, num in enumerate(nums)}
+                it saves the index of last occurrence of that number, giving us false data
+                Imp to check constraints!!
+        '''
         
+
