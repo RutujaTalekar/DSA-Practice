ba6b3f83a1368dd7489a9fd476533684b6ba3baa
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-05-02 01:50:37 -0400

    Time: 43 ms (94.82%) | Memory: 30 MB (75.47%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.py
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.py
@@ -7,7 +7,8 @@ class Solution:
         sort and return the min diff list. N square solution with added space
 
         Better approach - sort the array, and then check only consequent diffs
-        store them in hashmap - key is index and val is diff
+        store them in hashmap - either key - diff and val - list of pairs or 
+        key - index and val - diff
         Then for the min value, and create list of list
         '''
 
@@ -18,27 +19,22 @@ class Solution:
             diff = abs(arr[i]-arr[i+1])
             if diff <= minimum :
                 minimum = diff
+                map_result[diff].append([arr[i], arr[i+1]])
+
+        minimum = min(map_result.keys()) 
+        return map_result[minimum]
+
+
+        # other way of doing the same hashmap
+        map_result = defaultdict(list)
+        minimum = abs(arr[0] - arr[1])
+        arr.sort()
+        for i in range(len(arr)-1):
+            diff = abs(arr[i]-arr[i+1])
+            if diff <= minimum :
+                minimum = diff
                 map_result[i].append(diff)
 
         minimum = min(map_result.values()) 
         result = [[arr[i], arr[i+1]] for i, diff in map_result.items() if diff == minimum]
         return result
-
-
-
-
-
-
-
-        # map_result = defaultdict(list)
-        # minimum = abs(arr[0] - arr[1])  #we will have minimum 2 elements in arr
-        # arr.sort()
-        # for i in range(len(arr)-1):
-        #     diff = abs(arr[i]-arr[i+1])
-        #     if diff <= minimum :
-        #         minimum = diff
-        #         map_result[diff].append([arr[i], arr[i+1]])
-
-        # minimum = min(map_result.keys()) 
-        
-        # return map_result[minimum]
