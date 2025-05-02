eec5d8c2ffa0d44674a4adbd32140cc0c6621cf6
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-05-02 01:48:14 -0400

    Time: 53 ms (61.53%) | Memory: 30 MB (37.38%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.py
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.py
@@ -10,6 +10,26 @@ class Solution:
         store them in hashmap - key is index and val is diff
         Then for the min value, and create list of list
         '''
+
+        map_result = defaultdict(list)
+        minimum = abs(arr[0] - arr[1])  #we will have minimum 2 elements in arr
+        arr.sort()
+        for i in range(len(arr)-1):
+            diff = abs(arr[i]-arr[i+1])
+            if diff <= minimum :
+                minimum = diff
+                map_result[i].append(diff)
+
+        minimum = min(map_result.values()) 
+        result = [[arr[i], arr[i+1]] for i, diff in map_result.items() if diff == minimum]
+        return result
+
+
+
+
+
+
+
         map_result = defaultdict(list)
         minimum = abs(arr[0] - arr[1])  #we will have minimum 2 elements in arr
         arr.sort()
