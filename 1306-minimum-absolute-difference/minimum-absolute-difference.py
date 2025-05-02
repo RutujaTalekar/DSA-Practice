a4604f82ca7977bce63b21d3ebd640edd0fcee67
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-05-02 01:48:23 -0400

    Time: 53 ms (61.53%) | Memory: 30.1 MB (18.71%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.py
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.py
@@ -30,15 +30,15 @@ class Solution:
 
 
 
-        map_result = defaultdict(list)
-        minimum = abs(arr[0] - arr[1])  #we will have minimum 2 elements in arr
-        arr.sort()
-        for i in range(len(arr)-1):
-            diff = abs(arr[i]-arr[i+1])
-            if diff <= minimum :
-                minimum = diff
-                map_result[diff].append([arr[i], arr[i+1]])
+        # map_result = defaultdict(list)
+        # minimum = abs(arr[0] - arr[1])  #we will have minimum 2 elements in arr
+        # arr.sort()
+        # for i in range(len(arr)-1):
+        #     diff = abs(arr[i]-arr[i+1])
+        #     if diff <= minimum :
+        #         minimum = diff
+        #         map_result[diff].append([arr[i], arr[i+1]])
 
-        minimum = min(map_result.keys()) 
+        # minimum = min(map_result.keys()) 
         
-        return map_result[minimum]
+        # return map_result[minimum]
