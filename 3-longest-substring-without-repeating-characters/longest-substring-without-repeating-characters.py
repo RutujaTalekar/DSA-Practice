d4b749a03ccb14810f861b3109001eead96ee3a9
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-05-03 17:58:20 -0400

    Time: 19 ms (56.41%) | Memory: 17.8 MB (82.40%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -10,14 +10,13 @@ class Solution:
         cur = 0
         prev = cur
         while cur < len(s):
-            
+ 
             while s[cur] in unique:
                 unique.remove(s[prev])
                 prev += 1
 
             unique.add(s[cur])
             res = max(res, cur - prev + 1)
-            
             cur += 1
             
         return res
