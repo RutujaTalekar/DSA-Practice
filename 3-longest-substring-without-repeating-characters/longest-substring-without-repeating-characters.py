faa0e051bbf69239be63c8fa7067553320326a20
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-05-03 17:56:15 -0400

    Time: 19 ms (56.41%) | Memory: 17.8 MB (82.40%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,86 +1,32 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        set_s = set(s)
-        temp = set() # []
+        '''
+        Use sliding window pointers - prev, cur to keep track of substring
+        Use sets to keep track of unique characters from substring
+        '''
 
-        # au
-        if len(set_s) == 1:
-            return 1
-        # a b c a b c b b
-        # 0 1 2 3 4 5 6 7
-        # cur = 3
-        # prev = 0
-
-        result = 0
-        cur, prev = 0, 0
+        unique = set()
+        res = 0
+        cur = 0
+        prev = cur
         while cur < len(s):
-            while s[cur] in temp:
-                result = max(result, len(temp))
-                temp.remove(s[prev])
+            
+            while s[cur] in unique:
+                unique.remove(s[prev])
                 prev += 1
-                
-            temp.add(s[cur])
+
+            unique.add(s[cur])
+            res = max(res, len(unique))
+            
             cur += 1
-        result = max(result, len(temp))
-        
-        return result
-
-
-        # Two for loops
-        # new list
-        # check if the first element and next element not equal, add it to new array
-        # 
+            
+        return res
 
         
+
+            
 
 
 
 
-
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # characterSet = set()
-        # result = 0
-        # l = 0
-        # for r in range(len(s)):
-        #     while s[r] in characterSet:
-        #         characterSet.remove(s[l])
-        #         l+=1
-        #     characterSet.add(s[r])
-        #     result = max(result, r-l+1)
-        # return result
-        
-
+            
