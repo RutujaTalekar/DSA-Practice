2c70539f7d73ac56f42bb18e5dd4c181edc8827d
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-05-04 16:53:09 -0400

    Time: 19 ms (23.47%) | Memory: 35.1 MB (10.01%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,22 +1,18 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         # using hashsets
-        if len(nums) == len(set(nums)):
-            return False
-        return True
+        # if len(nums) == len(set(nums)):
+        #     return False
+        # return True
 
 
         # using counter map for frequency time - O(n)
         frequency = {}
         for num in nums:
             if num in frequency:
-                frequency[num] += 1
+                return True
             else:
                 frequency[num] = 1
-        
-        for num, freq in frequency.items():
-            if freq >= 2:
-                return True
         
         return False
 
