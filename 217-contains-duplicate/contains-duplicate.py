3f553607782badf5fcb3967c30b92887e5a0cf39
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-05-04 16:52:26 -0400

    Time: 11 ms (60.23%) | Memory: 31.7 MB (21.18%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,5 +1,12 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        # using hashsets
+        if len(nums) == len(set(nums)):
+            return False
+        return True
+
+
+        # using counter map for frequency time - O(n)
         frequency = {}
         for num in nums:
             if num in frequency:
@@ -12,4 +19,7 @@ class Solution:
                 return True
         
         return False
+
+
+
 
