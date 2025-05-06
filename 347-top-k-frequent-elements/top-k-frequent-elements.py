0949f8809d95cdc707b60405571e06df6676ac83
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-05-05 20:48:00 -0400

    Time: 3 ms (90.95%) | Memory: 21.4 MB (28.70%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,9 +1,26 @@
+import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # Solution 1, use Counter map
+
+        # Solution 2, use heap
+        # get frequencies
         frequency = Counter(nums)
-        res = sorted(frequency.keys(), key = lambda x: -frequency[x])  # sorts the keys based on values in descending order
-        return res[:k]
+        # make list of tuples
+        heap = [(freq, num) for num, freq in frequency.items()]
+        heap = heapq.nlargest(k, heap)
+        return [y for x,y in heap]
+
+        
+
+
+        # Solution 1, use Counter map and sorting by values
+        frequency = Counter(nums)
+        sorted_keys_by_values = sorted(frequency, key = lambda x: - frequency[x])  # sorts the keys based on values in descending order
+        return sorted_keys_by_values[:k]
+
+
+        
+        
 
 
 
