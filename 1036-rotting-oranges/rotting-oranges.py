5d08cc7ce75f5301f7d4c996ed35a98e84d10d59
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-05-06 01:16:33 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (71.67%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,73 +1,46 @@
 class Solution:
-
     def orangesRotting(self, grid: List[List[int]]) -> int:
         '''
         We could use BFS to solve this, as we are looking at adjacent cells
         and essentially counting how many loops (mins) it would take to cover all cells
         '''
+
+        # Before we can start BFS, we have to find which cells can initiate BFS i.e. which cells are rotten - append in queue
+        # Another thing to keep track of is, how many fresh oranges do we have?
+        # This counter will help us to return -1 if rotten oranges could not reach all of the fresh oranges
+
         rows = len(grid)
         cols = len(grid[0])
-
-        # first check which all cells have rotten oranges to start BFS on
         q = deque()
-        fresh_orange = 0
+        fresh = 0
+        time = 0
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
-        
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j] == 2:
                     q.append((i,j))
                 if grid[i][j] == 1:
-                    fresh_orange += 1
+                    fresh += 1
         
-        # edge case
-        # if there are no fresh oranges but rotten oranges are present then we did it 0 minutes!
-        if fresh_orange == 0:
-            return 0
+        # edge case - if there are no fresh oranges then we can return 0, cause it took us 0 mins to reach to that state
+        if fresh == 0:
+            return time
         
+        # lets starts the BFS, and keep it going untill we have nothing in queue or fresh oranges are covered
+        while q and fresh > 0:
+            for _ in range(len(q)):     # range function takes snap shot of length at that time, and does level by level traversal
+                i, j = q.popleft()
+                for dr, dc in directions:
+                    r = i + dr
+                    c = j + dc
+                    if r < rows and r >= 0 and c < cols and c >= 0 and grid[r][c] == 1:
+                        fresh -= 1
+                        q.append((r,c))
+                        grid[r][c] = 2
+            time += 1
         
-        # now start BFS
-        
-        minutes = -1
-        
-        while q:
-            minutes += 1
-            
-            for _ in range(len(q)):
-                r,c = q.popleft()
+        return time if fresh == 0 else -1
+
+
                 
-                if r+1 < rows and r+1 >= 0 and c < cols and c >= 0 and grid[r+1][c] == 1:
-                    q.append((r+1, c))
-                    fresh_orange -= 1
-                    grid[r+1][c] = 2
-
-                if r < rows and r >= 0 and c+1 < cols and c+1 >= 0 and grid[r][c+1] == 1:
-                    q.append((r,c+1))
-                    fresh_orange -= 1
-                    grid[r][c+1] = 2
-
-                if r < rows and r >= 0 and c-1 < cols and c-1 >= 0 and grid[r][c-1] == 1:
-                    q.append((r, c-1))
-                    fresh_orange -= 1
-                    grid[r][c-1] = 2
-
-                if r-1 < rows and r-1 >= 0 and c < cols and c >= 0 and grid[r-1][c] == 1:
-                    q.append((r-1, c))
-                    fresh_orange -= 1
-                    grid[r-1][c] = 2
-                    
-            
-        
-        if fresh_orange != 0:
-            return -1
-
-        return minutes
-
-
-                    
-                    
-
-
-
-
-
