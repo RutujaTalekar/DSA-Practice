694e629b3df8c4e388b8c352f9d39aec0d54a1e6
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-05-06 12:17:51 -0400

    Time: 13 ms (45.30%) | Memory: 17.7 MB (90.45%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -1,39 +1,17 @@
-class Solution(object):
-    def convert(self, s, numRows):
-        """
-        :type s: str
-        :type numRows: int
-        :rtype: str
-        """
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        res = ''
+        step = 0
 
-        '''
-        Play with indices to get zig zag pattern
-        First char -> char at 0, place in op
-        next char -> + (numRows + 2), and so on for first row
-        second row first char - > char at 1
-        next char -> i+ 4 and then i + 6 so on
-        third row -> i+ 2 and then i + 6 so on
-        '''
-
-        if len(s)<= 1 or numRows == 1:
+        if numRows == 1 or len(s) == 1:
             return s
 
-        res = ''
-        
         for row in range(numRows):
-            step = (numRows-1) * 2
+            step = 2*(numRows-1)
             for i in range(row, len(s), step):
                 res += s[i]
-                next_char = i + (step - 2*row)
-                if not( row == 0 or row == numRows - 1 ) and next_char < len(s):
-                    '''
-                    1 -> 4th    (step - 2* (1))
-                    2 -> 2nd    (step - 2* (2))
-                    '''
-                    res += s[next_char]
+                also_include_char_at = i + (step - 2*(row))
+                if not(row == 0 or row == numRows-1) and also_include_char_at < len(s):
+                    res += s[also_include_char_at]  
         
         return res
-                
-
-
-        
