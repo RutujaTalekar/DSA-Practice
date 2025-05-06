3e767b7b516b045bfb1e4b505f9aed7387010d56
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-05-05 23:11:58 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (39.29%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -1,41 +1,51 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def isSameTree(self, p, q):
-        """
-        :type p: Optional[TreeNode]
-        :type q: Optional[TreeNode]
-        :rtype: bool
-        """
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+
+        # cleaned up DFS
+        
+        if not p and not q:
+            return True
+        if p and q:
+            return (
+                p.val == q.val
+                and self.isSameTree(p.left, q.left)
+                and self.isSameTree(p.right, q.right)
+            )
+        return False
+        
+        # bfs iterative
+        '''
         if not p and not q:
             return True
         if not p or not q:
             return False
-
-        qu = deque()
-        qu.append((p,q))
+        
+        qu = deque([(p, q)])
 
         while qu:
-            for _ in range(len(qu)):
-                p, q = qu.popleft()
-                if not p:
-                    if q:
-                        return False
-                    continue
-                        
-                if not q:
-                    if p:
-                        return False
-                    continue
+            pNode, qNode = qu.popleft()
 
-                if p.val == q.val:
-                    qu.append((p.left,q.left))
-                    qu.append((p.right,q.right))
-                else:
-                    return False
+            if not pNode and not qNode:
+                continue
+            if not pNode or not qNode:
+                return False
+            if pNode.val != qNode.val:
+                return False
+
+            # Always enqueue both children, even if None
+            qu.append((pNode.left, qNode.left))
+            qu.append((pNode.right, qNode.right))
 
         return True
+
+        '''
+
+        
+
+        
