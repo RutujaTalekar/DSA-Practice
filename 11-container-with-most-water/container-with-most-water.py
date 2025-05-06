40f26088ab7c6d641f3c32464bc863eb87ea816c
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-05-06 01:48:57 -0400

    Time: 82 ms (87.72%) | Memory: 20.9 MB (14.34%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,35 +1,34 @@
-class Solution:
-    
-    def maxArea(self, height: List[int]) -> int:
-        
-        # Efficient two pointer approach O(n)
-        # place left and right pointers on extreme ends and calculate are to maximize
-        # calculate area of rectangle - length * breadth
-        # length is width right - left
-        # breadth is height of min side
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
 
-        result = 0
-        left, right = 0, len(height) - 1
-        while left < right:
-            h = min(height[left], height[right])
-            area = h * (right - left)
-            result = max(result, area)
-            # dont forget to increment pointers, increment whatever was used
-            if height[left] == height[right]:
+        '''
+        Can start from extreme ends cause logically that rectangle would have most width i.e. more water
+
+        '''
+
+        left, right = 0, len(height)-1
+        area = - float('inf')
+        while left< right:
+            if height[left] < height[right]:
+                l = height[left]
+                b = right - left
                 left += 1
-                right -= 1
-            elif height[left] < height[right]:
-                left += 1
+
             else:
-                right -= 1
-        return result
+                l = height[right]
+                b = right - left
+                right-=1
 
-        # Brute force - Time limit exceeded O(n square)
-        result = 0
-        for i in range(len(height)):
-            for j in range(i+1, len(height)):
-                area = (j-i) * min(height[j],height[i])
-                result = max(result, area)
-        
-        return result
             
+            area = max(area, l*b)
+        
+        return area
+            
+
+
+
+        
