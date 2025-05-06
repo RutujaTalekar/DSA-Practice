4f4d6a118673b423f7ea62405ba812fb7dd32d67
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-05-05 22:40:41 -0400

    Time: 4 ms (39.16%) | Memory: 15.1 MB (42.35%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -1,40 +1,17 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        
-        # BFS Iterative
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
         if not root:
             return 0
-        
-        q = deque([root])
-        level = 0
-        while q:
-            level += 1
-            for _ in range(len(q)):
-                node = q.popleft()
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-        
-        return level
+        return 1 +  max(self.maxDepth(root.left) , self.maxDepth(root.right))
 
-
-
-
-        # DFS Recursive
-        # base case
-        if not root:
-            return 0
-
-
-        # recursively check - 
-        # height at root = 1 + max(left tree, right tree)
-        # 1 is counting the root itself
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
         
