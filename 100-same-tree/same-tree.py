d281e040d4b077d448b6839cb6e5b18d1ce9c3d7
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-05-05 22:54:19 -0400

    Time: 0 ms (100.00%) | Memory: 12.7 MB (0.07%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -1,51 +1,26 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-
-        # cleaned up DFS
-        
-        if not p and not q:
-            return True
-        if p and q:
-            return (
-                p.val == q.val
-                and self.isSameTree(p.left, q.left)
-                and self.isSameTree(p.right, q.right)
-            )
-        return False
-        
-        # bfs iterative
-        '''
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: Optional[TreeNode]
+        :type q: Optional[TreeNode]
+        :rtype: bool
+        """
         if not p and not q:
             return True
         if not p or not q:
             return False
         
-        qu = deque([(p, q)])
+        if p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right,q.right):
+            return True
+        else:
+            return False
+            
 
-        while qu:
-            pNode, qNode = qu.popleft()
-
-            if not pNode and not qNode:
-                continue
-            if not pNode or not qNode:
-                return False
-            if pNode.val != qNode.val:
-                return False
-
-            # Always enqueue both children, even if None
-            qu.append((pNode.left, qNode.left))
-            qu.append((pNode.right, qNode.right))
-
-        return True
-
-        '''
-
-        
 
         
