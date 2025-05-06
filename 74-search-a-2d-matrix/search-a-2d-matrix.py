9c67418232610a15aeeeb72b51171aeb90c5d840
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-05-06 16:47:12 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (99.12%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,6 +1,36 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # lets apply binary search on cols as well as rows 
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        top, bottom = 0, rows - 1 
+        while top <= bottom:
+            mid_row = (top + bottom)//2
+            if target > matrix[mid_row][-1]:
+                top = mid_row + 1
+                
+            elif target < matrix[mid_row][0]:
+                bottom = mid_row - 1
+                
+            else:
+                # then target value is actually in this loop, then break and start doing bin search on that row
+                break
         
+        left, right = 0, cols -1
+        # mid_row = (top + bottom)//2
+        while left <= right:
+            mid = (left + right) // 2
+            if target == matrix[mid_row][mid]:
+                return True
+            elif target < matrix[mid_row][mid]:
+                right = mid -1
+            else:
+                left = mid + 1
+        
+        return False
+
+
         
         # lets think binary search
         rows = len(matrix)
