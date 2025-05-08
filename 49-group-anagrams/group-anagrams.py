8e2ed876fa1ea182ec42d9063a1dcc70759d5925
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-05-08 11:35:18 -0400

    Time: 23 ms (18.92%) | Memory: 22.7 MB (25.13%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -2,15 +2,26 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
         # O(n * m)
-        '''
+
+        # ans = collections.defaultdict(list)
+        # for s in strs:
+        #     count = [0] * 26
+        #     for c in s:
+        #         count_temp = ord(c) - ord("a")
+        #         count[ord(c) - ord("a")] += 1
+        #     ans[tuple(count)].append(s)
+        # return list(ans.values())
+        
         ans = collections.defaultdict(list)
         for s in strs:
-            count = [0] * 26
-            for c in s:
-                count_temp = ord(c) - ord("a")
-                count[ord(c) - ord("a")] += 1
-            ans[tuple(count)].append(s)
+            temp = [0] * 26
+            for char in s:
+                asci = ord('a') - ord(char)
+                temp[asci] += 1
+            ans[tuple(temp)].append(s)
+        
         return list(ans.values())
+
         '''
 
         # O(n * m * log m)
@@ -18,18 +29,19 @@ class Solution:
         for s in strs:
             ans[tuple(sorted(s))].append(s)
         return list(ans.values())
-        
+        '''
 
 
         # Draft 1 solution
         '''
         dict_map = defaultdict(list)
-        for char in strs:
-            key = ''.join(sorted(char))
-            dict_map[key].append(char)
+        for word in strs:
+            key = ''.join(sorted(word))
+            dict_map[key].append(word)
+
+        
         return list(dict_map.values())
         '''
-        
         
             
 
