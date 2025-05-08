fd2505588cfb7f8003ff5076ce987de4d9d38947
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-05-08 11:51:49 -0400

    Time: 0 ms (100.00%) | Memory: 21.1 MB (89.85%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -10,29 +10,29 @@ class Solution:
         But for solution 2 - we store O(n)  
         '''
         # Solution 3, use heap - but efficient
-        frequency = Counter(nums)
-        min_heap = []
+        # frequency = Counter(nums)
+        # min_heap = []
 
-        for num, freq in frequency.items():
-            heapq.heappush(min_heap, (freq,num))
-            if len(min_heap) > k:
-                heapq.heappop(min_heap)
+        # for num, freq in frequency.items():
+        #     heapq.heappush(min_heap, (freq,num))
+        #     if len(min_heap) > k:
+        #         heapq.heappop(min_heap)
         
-        return [num for freq, num in min_heap]
+        # return [num for freq, num in min_heap]
 
 
-        # Solution 2, use heap
-        frequency = Counter(nums)
-        heap = [(freq, num) for num, freq in frequency.items()]
-        top_k = heapq.nlargest(k, heap)
-        return [num for freq, num in top_k]
+        # # Solution 2, use heap
+        # frequency = Counter(nums)
+        # heap = [(freq, num) for num, freq in frequency.items()]
+        # top_k = heapq.nlargest(k, heap)
+        # return [num for freq, num in top_k]
 
         
 
 
         # Solution 1, use Counter map and sorting by values
-        frequency = Counter(nums)
-        sorted_keys_by_values = sorted(frequency, key = lambda x: - frequency[x])  # sorts the keys based on values in descending order
+        frequency = Counter(nums)        
+        sorted_keys_by_values = sorted(frequency.keys(), key = lambda x: - frequency[x])  # sorts the keys based on values in descending order
         return sorted_keys_by_values[:k]
 
 
