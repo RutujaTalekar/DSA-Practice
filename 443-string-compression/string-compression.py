3ed2d863914e79d7daafd03302f4f45fe5708df7
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-05-08 14:21:24 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (94.84%) - LeetSync

---
+++ 443-string-compression/string-compression.py
@@ -0,0 +1,27 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        i = 0
+        n = len(chars)
+        insert_at = 0
+
+        while i < n:
+            char = chars[i]
+            freq = 1
+
+            while i+freq < n and char == chars[i+freq]:
+                freq += 1
+            
+            chars[insert_at] = char
+            insert_at += 1
+
+            if freq > 1:
+                chars[insert_at: insert_at + len(str(freq))] = list(str(freq))
+                insert_at += len(str(freq))
+            
+            i+= freq
+        
+        chars = chars[:insert_at]
+        return len(chars)
+
+
+        
