d2545853e912081ea3a0717d1c2311507bca7674
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-05-09 10:06:40 -0400

    Time: 31 ms (34.48%) | Memory: 19.4 MB (71.33%) - LeetSync

--- 697-degree-of-an-array/degree-of-an-array.py
+++ 697-degree-of-an-array/degree-of-an-array.py
@@ -3,8 +3,8 @@ from collections import defaultdict
 class Solution:
     def findShortestSubArray(self, nums: List[int]) -> int:
         count = Counter(nums)   # to store frequency map of each num
-        left = {}               # to store the first occurance of each num
-        right = {}              # to store current index and calculate result 
+        left = {}               # to store the first seen index of each num
+        right = {}              # to store the last seen index of each num
 
         degree = max(count.values())
         result = math.inf
@@ -13,7 +13,7 @@ class Solution:
             if nums[i] not in left:
                 left[nums[i]] = i
             right[nums[i]] = i
-            
+
         for i in range(len(nums)):
             if count[nums[i]] == degree:
                 length = right[nums[i]] - left[nums[i]] + 1
