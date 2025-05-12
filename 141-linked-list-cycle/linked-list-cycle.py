af8437bd4f241c9221e14eb8c10a7883bf666937
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-05-12 19:01:29 -0400

    Time: 60 ms (5.59%) | Memory: 20.2 MB (11.51%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -6,7 +6,20 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
+        '''
+        Floyd's cycle detection - use slow and fast pointers to check cycle
+        Time - O(n) and space - O(1)
+        '''
+        # if head is None:
+        #     return False
 
+
+
+        
+        '''
+        Using set to check visited nodes
+        Time & Space - O(n)
+        '''
         visited = set()
 
         while head:
