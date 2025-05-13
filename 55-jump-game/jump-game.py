8207ac2f6ba99aa20cdd70efed4c275d0fbfd1b2
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-05-13 18:39:45 -0400

    Time: 13 ms (91.64%) | Memory: 13.4 MB (52.00%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,43 +1,25 @@
-class Solution:
-    def canJump(self, nums: List[int]) -> bool:
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+
         '''
-        Greedy approach - instead of starting from 0 idx and reaching last idx as goal
-        lets start from end and reach 0 as a goal
-
-        At every poisition from the second last, check if you can reach the last idx
-        If you can - then your goal is now changed to reaching that particular idx
-
-        Initialize goal ptr to len of nums
-        Traverse reverse
-        If we can reach the goal from cur idx we can shift the goal cur idx
+        Greedy approach 
+        Time - O(n) Space - O(1)
         '''
         goal = len(nums)-1
+        for i in range(len(nums)-2, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+            
+        return True if goal == 0 else False
 
-        for idx in range(len(nums)-1, -1, -1):
-            if idx + nums[idx] >= goal:
-                goal = idx
         
-        return True if goal==0 else False
-        
-        '''
-        You can use dynamic programming or greedy approach
-        With dynamic programming and memoization, you need O(n square) time and O(n) space
-        With greedy, you need O(n) time and O(1) space
-        '''
-        n = len(nums)
-        dp = [False] * n
-        dp[-1] = True  # We can always reach the end from the last index
-
-        for i in range(n - 2, -1, -1):
-            furthestJump = min(i + nums[i], n - 1)
-            for j in range(i + 1, furthestJump + 1):
-                if dp[j]:
-                    dp[i] = True
-                    break
-
-        return dp[0]
 
 
+            
 
 
 
