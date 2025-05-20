af6143a2cf29c128d91949b05d6929673451ab6b
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-05-20 00:10:47 -0400

    Time: 1 ms (3.63%) | Memory: 17.8 MB (48.86%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,18 +1,18 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
         # dynamic programming with optimized storage
-        t0,t1,t2 = 0,1,1
-        if n == 0:
-            return t0
-        if n < 3:
-            return t1
-        tn = 0
-        i = 3
-        while i <= n:
-            tn = t0+t1+t2
-            t0,t1,t2 = t1,t2,tn
-            i+=1
-        return tn
+        # t0,t1,t2 = 0,1,1
+        # if n == 0:
+        #     return t0
+        # if n < 3:
+        #     return t1
+        # tn = 0
+        # i = 3
+        # while i <= n:
+        #     tn = t0+t1+t2
+        #     t0,t1,t2 = t1,t2,tn
+        #     i+=1
+        # return tn
 
         # recursive - optimized 
         mem = [0]* (n+1)
