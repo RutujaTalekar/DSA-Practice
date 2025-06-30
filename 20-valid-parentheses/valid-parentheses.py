002db5f69e4d273a8de7eb3b93e28d59719d6114
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-06-29 23:33:35 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (80.46%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,38 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # lookup = {')':'(', '}':'{', ']':'['}
-        # stack = []
-
-        # for char in s:
-        #     if char in lookup:
-        #         if stack and stack[-1] == lookup[char]:
-        #             stack.pop()
-        #         else:
-        #             return False
-        #     else:
-        #         stack.append(char)
-        
-        # return True if not stack else False
-
-
-        
-        map = { ')':'(' , '}':'{' , ']':'['  }
+        lookup = {')':'(', '}':'{', ']':'['}
         stack = []
 
-        for key in s:
-            if key in map.values():
-                stack.append(key)
-
-            else:
-                if stack:
-                    if stack[-1]!= map[key]:
-                        return False
-                    stack.pop() #matching pair\
+        for char in s:
+            if char in lookup:
+                if stack and stack[-1] == lookup[char]:
+                    stack.pop()
                 else:
                     return False
-
-        return True if not stack else False
+            else:
+                stack.append(char)
         
+        return True if not stack else False
+
             
 
 
