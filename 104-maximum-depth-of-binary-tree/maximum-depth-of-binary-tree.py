c89e55c3b2114349cb807c38034a29910aa5d829
Rutuja Talekar <49370316+RutujaTalekar@users.noreply.github.com>
2025-07-09 22:08:36 -0400

    Time: 0 ms (100.00%) | Memory: 14.9 MB (86.85%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -1,38 +1,33 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
 
-        # DFS Recursive
-        # base case
         if not root:
             return 0
 
-        # recursively check - 
-        # height at root = 1 + max(left tree, right tree)
-        # 1 is counting the root itself
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
-
-                
-        # BFS Iterative
-        '''
-        if not root:
-            return 0
-        
-        q = deque([root])
-        level = 0
+        q = deque()
+        q.append(root)
+        depth = 0
         while q:
-            level += 1
+            depth +=1
+            # node = q.popright()
             for _ in range(len(q)):
                 node = q.popleft()
                 if node.left:
                     q.append(node.left)
                 if node.right:
                     q.append(node.right)
-        return level
-        '''
-        
+        return depth
+                
+
+
+
